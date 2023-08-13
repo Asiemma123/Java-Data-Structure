e6b2c62b06732c533bee9232e6a77e7eec372d81
Asiemma123 <132608241+Asiemma123@users.noreply.github.com>
2023-08-13 00:38:43 -0700

    Add files via upload

---
+++ Data Structure/src/ArraySum.java
@@ -0,0 +1,18 @@
+
+public class ArraySum {
+	
+public static void main(String []args){
+        
+        int arr[] = {3,8,7,919};
+        int sum=0;
+        
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        
+        System.out.println("Total sum of array is: "+sum);
+        
+     }
+}
+
+
